1e0b44ee40a6c82a2ccd80cee3830b86d6684b14
BryceMatthes <bryce@mathx.ca>
2019-02-07 13:22:58 -0500

    Vote Changes 2.3.2

--- src/common/setIntercepts.ts
+++ src/common/setIntercepts.ts
@@ -1,7 +1,7 @@
 
 const redirects = [
   {
-    from: '*://*.destiny.gg/*/chat.js',
+    from: 'https://cdn.destiny.gg/chat.*.js',
     to: './dgg/chat.js',
   },
   {
@@ -15,6 +15,10 @@ redirects.forEach(({ from, to }) => {
     const redirect = {
       redirectUrl: chrome.extension.getURL(to),
     };
+    if(details.url.includes('vendor')){
+    	console.log("skipped match of vendor");
+	return { redirectUrl: details.url }
+    }
     console.log(`redirecting from "${from}" to "${to}"`, redirect);
     return redirect;
   }, {
